2fafb7b016aeb032842a4588128a7f7f87df23c7
romanyshyn97 <romanyshyn.ol.rom@gmail.com>
2022-04-28 20:23:42 +0300

    sending data by formData and json

--- js/main.js
+++ js/main.js
@@ -207,5 +207,60 @@ window.addEventListener('DOMContentLoaded' , ()=>{
         19,
         '.menu .container'
     ).render();
-    
+
+    //FORMS
+
+    const forms = document.querySelectorAll('form');
+
+    const message = {
+        loading: 'loading',
+        success: 'we will contact you soon',
+        failure: 'something goes wrong'
+    };
+
+    forms.forEach(item => {
+        postData(item);
+    }); 
+
+    function postData(form){
+        form.addEventListener('submit', (e) => {
+            e.preventDefault();
+
+            const statusMessage = document.createElement('div');
+            statusMessage.classList.add('status');
+            statusMessage.textContent = message.loading;
+            form.append(statusMessage);
+
+            const request = new XMLHttpRequest();
+            request.open('POST', 'server.php');
+
+            request.setRequestHeader('Content-type', 'application/json');
+            const formData = new FormData(form);
+
+            const object = {};
+            formData.forEach(function(value, key){
+                object[key] = value;
+            });
+
+            const json = JSON.stringify(object);
+
+            request.send(json);
+
+            request.addEventListener('load', () => {
+                if (request.status === 200) {
+                    console.log(request.response);
+                    statusMessage.textContent = message.success;
+                    form.reset(); // очистка формы
+                    setTimeout(() => { // сброс статуса
+                        statusMessage.remove();
+                    },2000);
+                }
+                else{
+                    statusMessage.textContent = message.failure;
+                }
+
+            });
+
+        });
+    }
 });
