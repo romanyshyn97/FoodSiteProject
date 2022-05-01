c88d08235eaa37007d1ab68eae7d926fb2ec6901
romanyshyn97 <romanyshyn.ol.rom@gmail.com>
2022-05-01 19:24:04 +0300

    change xmlhttp to fetch

--- js/main.js
+++ js/main.js
@@ -236,9 +236,10 @@ window.addEventListener('DOMContentLoaded' , ()=>{
             // form.append(statusMessage);
             form.insertAdjacentElement('afterend', statusMessage)
 
-            const request = new XMLHttpRequest();
-            request.open('POST', 'server.php');
-            request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+            
+
+           
+            
             const formData = new FormData(form);
 
             const object = {};
@@ -246,23 +247,24 @@ window.addEventListener('DOMContentLoaded' , ()=>{
                 object[key] = value;
             });
 
-            const json = JSON.stringify(object);
-
-            request.send(json);
-
-            request.addEventListener('load', () => {
-                if (request.status === 200) {
-                    console.log(request.response);
-                    showThanksModal(message.success);
-                    statusMessage.remove();
-                    form.reset(); 
-                    
-                }
-                else{
-                    showThanksModal(message.failure);
-                }
-
-            });
+            fetch('server.php', {
+                method: 'POST',
+                headers: {
+                    'Content-type': 'application/json'
+                },
+                body: JSON.stringify(object)
+            })
+            .then(data => data.text())
+            .then(data => {
+                console.log(data);
+                showThanksModal(message.success);
+                statusMessage.remove();
+            }).catch(() => {
+                showThanksModal(message.failure);
+            }).finally(() => {
+                form.reset();
+            })
+            
 
         });
     }
